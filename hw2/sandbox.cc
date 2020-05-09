c8fd54f9a61f71a700330f38509ab121b818e928
yuwei.shih.tw@gmail.com <yuwei.shih.tw@gmail.com>
2020-05-09 20:16:57 +0800

    refine arg

--- hw2/sandbox.cc
+++ hw2/sandbox.cc
@@ -1,37 +1,35 @@
 #include <cstdio>
 #include <cstdlib>
 #include <getopt.h>
-#include <regex> 
 #include <string.h>
-#include <string>
-#include <iostream>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    char *wrong_msg = "usage: ./sandbox [-p sopath] [-d basedir] [--] cmd [cmd args ...] \r\n\
+    const char *wrong_msg = "usage: ./sandbox [-p sopath] [-d basedir] [--] cmd [cmd args ...] \r\n\
         -p: set the path to sandbox.so, default = ./sandbox.so \r\n\
         -d: the base directory that is allowed to access, default = . \r\n\
         --: seperate the arguments for sandbox and for the executed command \r\n";
     
-    string cmd = "";
-    string so_path = "./sandbox.so";
-    string dir_path = "./";
+    char cmd[] = "";
+    char so[] = "./sandbox.so";
+    char dir[] = "./";
 
-    const char *opt_sring = "p:d:-";
+    const char *opt_sring = "p:d:-:";
     int opt;
     while((opt = getopt(argc, argv, opt_sring)) != -1) {
         switch(opt) {
             case 'd':
-                dir_path = string(optarg);
-                cout << dir_path << endl;
+                strcpy(dir, optarg);
+                printf("dir: %s\n", dir);
                 break;
             case 'p':
-                so_path = string(optarg);
-                cout << so_path << endl;
+                strcpy(so, optarg);
+                printf("so: %s\n", so);
                 break;
             case '-':
-                cmd = string(optarg);
+                strcpy(cmd, optarg);
+                printf("cmd: %s\n", cmd);
                 break;
             default:
                 printf("%s", wrong_msg);
