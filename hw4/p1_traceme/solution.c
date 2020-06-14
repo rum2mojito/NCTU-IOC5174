d0dfc5703934d2b330873592e2f82dd9679ff1c4
yuwei.shih.tw@gmail.com <yuwei.shih.tw@gmail.com>
2020-06-14 10:49:22 +0800

    format

--- hw4/p1_traceme/solution.c
+++ hw4/p1_traceme/solution.c
@@ -1,58 +1,59 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <sys/ptrace.h>
+#include <sys/types.h>
 #include <sys/user.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 void errquit(const char *msg) {
-	perror(msg);
-	exit(-1);
+  perror(msg);
+  exit(-1);
 }
 
 int main(int argc, char *argv[]) {
-	pid_t child;
-	if(argc < 2) {
-		fprintf(stderr, "usage: %s program\n", argv[0]);
-		return -1;
-	}
-	if((child = fork()) < 0) errquit("fork");
-	if(child == 0) {
-		if(ptrace(PTRACE_TRACEME, 0, 0, 0) < 0) errquit("ptrace");
-		execvp(argv[1], argv+1);
-		errquit("execvp");
-	} else {
-		long long counter = 0LL;
-		int wait_status;
-		if(waitpid(child, &wait_status, 0) < 0) errquit("wait");
-		ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_EXITKILL);
-		while(WIFSTOPPED(wait_status)) {
-			long ret;
-			unsigned long long rip;
-			struct user_regs_struct regs;
-			unsigned char *ptr = (unsigned char *) &ret;
-			counter++;
+  pid_t child;
+  if (argc < 2) {
+    fprintf(stderr, "usage: %s program\n", argv[0]);
+    return -1;
+  }
+  if ((child = fork()) < 0) errquit("fork");
+  if (child == 0) {
+    if (ptrace(PTRACE_TRACEME, 0, 0, 0) < 0) errquit("ptrace");
+    execvp(argv[1], argv + 1);
+    errquit("execvp");
+  } else {
+    long long counter = 0LL;
+    int wait_status;
+    if (waitpid(child, &wait_status, 0) < 0) errquit("wait");
+    ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_EXITKILL);
+    while (WIFSTOPPED(wait_status)) {
+      long ret;
+      unsigned long long rip;
+      struct user_regs_struct regs;
+      unsigned char *ptr = (unsigned char *)&ret;
+      counter++;
 #if USE_PEEKUSER
-			if((rip = ptrace(PTRACE_PEEKUSER, child, ((unsigned char *) &regs.rip) - ((unsigned char *) &regs), 0)) != 0) {
+      if ((rip = ptrace(PTRACE_PEEKUSER, child,
+                        ((unsigned char *)&regs.rip) - ((unsigned char *)&regs),
+                        0)) != 0) {
 #else
-			if(ptrace(PTRACE_GETREGS, child, 0, &regs) == 0) {
-				rip = regs.rax;
+      if (ptrace(PTRACE_GETREGS, child, 0, &regs) == 0) {
+        rip = regs.rax;
 #endif
-				ret = ptrace(PTRACE_PEEKTEXT, child, rip, 0);
-				if((rip & 0xfff) == 0xb80) {
-					for(int i=0; i<37; i++) {
-						printf(" [%i] %c", i, ptr[i]);
-					}
-					printf("\n");
-				}
-				
-			}
-			if(ptrace(PTRACE_SINGLESTEP, child, 0, 0) < 0) errquit("ptrace@parent");
-			if(waitpid(child, &wait_status, 0) < 0) errquit("wait");
-		}
-		fprintf(stderr, "## %11d instruction(s) executed\n", counter);
-	}
-	return 0;
+        ret = ptrace(PTRACE_PEEKTEXT, child, rip, 0);
+        if ((rip & 0xfff) == 0xb80) {
+          for (int i = 0; i < 37; i++) {
+            printf(" [%i] %c", i, ptr[i]);
+          }
+          printf("\n");
+        }
+      }
+      if (ptrace(PTRACE_SINGLESTEP, child, 0, 0) < 0) errquit("ptrace@parent");
+      if (waitpid(child, &wait_status, 0) < 0) errquit("wait");
+    }
+    fprintf(stderr, "## %11d instruction(s) executed\n", counter);
+  }
+  return 0;
 }
--- hw4/p1_traceme/traceme2.c
+++ hw4/p1_traceme/traceme2.c
@@ -1,50 +1,47 @@
 #include <assert.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
+#include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/ptrace.h>
+#include <unistd.h>
 
-static unsigned char data[] = {
-...
-};
+static unsigned char data[] = {...};
 static char output[64];
 
 static int traced = 1;
 
-int f(int n) { 
-//	commented out
-//	commented out
-//	commented out
-//	commented out
-//	commented out
-//	commented out
-//	commented out
-//	commented out
-//	commented out
-//	commented out
+int f(int n) {
+  //	commented out
+  //	commented out
+  //	commented out
+  //	commented out
+  //	commented out
+  //	commented out
+  //	commented out
+  //	commented out
+  //	commented out
+  //	commented out
 }
 
 void handler(int s) {
-	traced = 0;
-	signal(SIGTRAP, SIG_DFL);
-	printf("trace me, please.\n");
-	exit(0);
+  traced = 0;
+  signal(SIGTRAP, SIG_DFL);
+  printf("trace me, please.\n");
+  exit(0);
 }
 
 int main(int argc, char *argv[]) {
-	pid_t child;
-	int i;
-	char buf[64];
-	signal(SIGTRAP, handler);
-	raise(SIGTRAP);
-	printf("traced\n");
-	for(i = 0; i < 37; i++) {
-		output[i] = data[1337 + f(i)];
-	}
-	output[i] = '\0';
-	return 0;
+  pid_t child;
+  int i;
+  char buf[64];
+  signal(SIGTRAP, handler);
+  raise(SIGTRAP);
+  printf("traced\n");
+  for (i = 0; i < 37; i++) {
+    output[i] = data[1337 + f(i)];
+  }
+  output[i] = '\0';
+  return 0;
 }
-
